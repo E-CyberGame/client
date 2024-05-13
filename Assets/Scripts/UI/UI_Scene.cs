b60f5d4608b37fc8087fa2e1bf68dda12ea3d789
Song Ein <songein@ewhain.net>
2024-05-13 21:01:39 +0900

    0513: 씬 추가
    씬 추가 및 Button Binding 작업

---
+++ Assets/Scripts/UI/UI_Scene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Scene : UI_Base
+{
+    public override void Init()
+    {
+    }
+}
