7fe71ea294d89ef08133cf9a5a5c4ef466565605
Song Ein <songein@ewhain.net>
2024-06-30 17:46:23 +0900

    0630: 스크립트와 UI 연결
    - CharacterManaging Scene의 UI와 스크립트 연결

--- Assets/Scripts/UI/UI_CharacterManaging.cs
+++ Assets/Scripts/UI/UI_CharacterManaging.cs
@@ -11,7 +11,12 @@ public class UI_CharacterManaging : UI_Popup
 {
     enum Buttons
     {
-        BackButton
+        BackButton,
+        Character1,
+        Character2,
+        Character3,
+        Character4,
+        Skill1, Skill2, Skill3, Skill4, Skill5
     }
 
     private void Start()
@@ -25,6 +30,15 @@ public class UI_CharacterManaging : UI_Popup
         Bind<Button>(typeof(Buttons));
 
         GetButton((int)Buttons.BackButton).gameObject.BindUIEvent(BackButtonClicked);
+        GetButton((int)Buttons.Character1).gameObject.BindUIEvent(CharacterButtonClicked);
+        GetButton((int)Buttons.Character2).gameObject.BindUIEvent(CharacterButtonClicked);
+        GetButton((int)Buttons.Character3).gameObject.BindUIEvent(CharacterButtonClicked);
+        GetButton((int)Buttons.Character4).gameObject.BindUIEvent(CharacterButtonClicked);
+        GetButton((int)Buttons.Skill1).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Skill2).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Skill3).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Skill4).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Skill5).gameObject.BindUIEvent(SkillButtonClicked);
 
     }
     public void BackButtonClicked(PointerEventData eventData)
@@ -32,4 +46,14 @@ public class UI_CharacterManaging : UI_Popup
         Debug.Log("BackButton Clicked");
         SceneManager.LoadScene("MainRoom");
     }
+    public void CharacterButtonClicked(PointerEventData eventData)
+    {
+        Debug.Log("CharacterButton Clicked");
+    }
+    public void SkillButtonClicked(PointerEventData eventData)
+    {
+        Debug.Log("SkillButton Clicked");
+        SceneManager.LoadScene("SkillManaging");
+    }
+
 }
