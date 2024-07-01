2bc2393a83072d56d9407a4771f79489cc610b70
Song Ein <songein@ewhain.net>
2024-07-01 14:37:44 +0900

    0701: SkillManagin UI 스크립트 연동
    - SkillPopup과 SkillTrainPopup을 스크립트에 추가 및 연동

--- Assets/Scripts/UI/UI_SkillManaging.cs
+++ Assets/Scripts/UI/UI_SkillManaging.cs
@@ -6,17 +6,31 @@ using System;
 using TMPro;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class UI_SkillManaging : UI_Popup
 {
+    private GameObject skillPopup;
+    private GameObject skillTrainPopup;
+
     enum Buttons
     {
-        BackButton
+        BackButton,
+        SPSk1,
+        SPSk2,
+        SPSk3,
+        Sk1, Sk2, Sk3, Sk4, Sk5, Sk6, Sk7, Sk8, Sk9,
+        SkillPopupClose, SkillTrainButton,
+        TrainCancelButton, TrainAcceptButton
     }
 
     private void Start()
     {
+        skillPopup = GameObject.Find("SkillPopup");
+        skillTrainPopup = GameObject.Find("SkillTrainPopup");
         Init();
+        skillPopup.SetActive(false);
+        skillTrainPopup.SetActive(false);
     }
 
     public override void Init()
@@ -25,11 +39,51 @@ public class UI_SkillManaging : UI_Popup
         Bind<Button>(typeof(Buttons));
 
         GetButton((int)Buttons.BackButton).gameObject.BindUIEvent(BackButtonClicked);
-
+        GetButton((int)Buttons.SPSk1).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.SPSk2).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.SPSk3).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Sk1).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Sk2).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Sk3).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Sk4).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Sk5).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Sk6).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Sk7).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Sk8).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.Sk9).gameObject.BindUIEvent(SkillButtonClicked);
+        GetButton((int)Buttons.SkillPopupClose).gameObject.BindUIEvent(PopupCloseButtonClicked);
+        GetButton((int)Buttons.SkillTrainButton).gameObject.BindUIEvent(SkillTrainButtonClicked);
+        GetButton((int)Buttons.TrainCancelButton).gameObject.BindUIEvent(TrainCancelButtonClicked);
+        GetButton((int)Buttons.TrainAcceptButton).gameObject.BindUIEvent(TrainAcceptButtonClicked);
     }
     public void BackButtonClicked(PointerEventData eventData)
     {
         Debug.Log("BackButton Clicked");
         SceneManager.LoadScene("CharacterManaging");
+    }
+    public void SkillButtonClicked(PointerEventData eventData)
+    {
+        Debug.Log("SkillButton Clicked");
+        skillPopup.SetActive(true);
+    }
+    public void PopupCloseButtonClicked(PointerEventData eventData)
+    {
+        Debug.Log("PopupCloseButton Clicked");
+        skillPopup.SetActive(false);
+    }
+    public void SkillTrainButtonClicked(PointerEventData eventData)
+    {
+        Debug.Log("SkillTrainButton Clicked");
+        skillTrainPopup.SetActive(true);
+    }
+    public void TrainCancelButtonClicked(PointerEventData eventData)
+    {
+        Debug.Log("TrainCancelButton Clicked");
+        skillTrainPopup.SetActive(false);
+    }
+    public void TrainAcceptButtonClicked(PointerEventData eventData)
+    {
+        Debug.Log("TrainAcceptButton Clicked");
+        skillTrainPopup.SetActive(false);
     }
 }
