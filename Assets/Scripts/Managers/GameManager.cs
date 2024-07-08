760e103eb4b9454b0d8f8132caa6a4e40a16542e
강승연 <syeon.dev@gmail.com>
2024-07-08 16:53:20 +0900

    [UPDATE] 기존 싱글톤-> 상속 싱글톤으로 중복 코드 제거

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,36 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Managers;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    private static GameManager _instance;
-    public static GameManager Instance { get { Init(); return _instance; } }
-    
     private void Awake()
     {
-        //Safety Check
-        if(Init())
-            Destroy(this.gameObject);
-    }
-
-    static bool Init()
-    {
-        if (_instance == null)
-        {
-            GameObject obj = GameObject.Find("@GameManager");
-            if (obj == null)
-            {
-                obj = new GameObject { name = "@GameManager" };
-                obj.AddComponent<GameManager>();
-            }
-
-            DontDestroyOnLoad(obj);
-            _instance = obj.GetComponent<GameManager>();
-
-            return false;
-        }
-
-        return true;
+        base.Awake();
     }
 }
