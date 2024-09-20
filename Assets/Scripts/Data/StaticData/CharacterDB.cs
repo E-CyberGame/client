7d4b3374dd57664996b8a28e04d53b547d281dfc
강승연 <syeon.dev@gmail.com>
2024-09-20 16:08:28 +0900

    [DATA] Scene, Character Data Script

---
+++ Assets/Scripts/Data/StaticData/CharacterDB.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Data
+{
+    public class CharacterDB
+    {
+        private Dictionary<CharacterType, CharacterData> _data;
+        
+        public CharacterDB(Dictionary<CharacterType, CharacterData> data)
+        {
+            _data = data;
+        }
+
+        public CharacterData GetData(CharacterType key)
+        {
+            return _data[key];
+        }
+        
+        public CharacterData GetData(int key)
+        {
+            return _data[(CharacterType)key];
+        }
+        
+        public int Count()
+        {
+            return _data.Count;
+        }
+    }
+}
