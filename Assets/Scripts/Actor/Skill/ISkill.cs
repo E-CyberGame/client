fc9287575ec36449beb5c30ca283db2d273ac5f4
강승연 <syeon.dev@gmail.com>
2024-10-14 17:23:54 +0900

    [REFACTOR] 쿨타임 UI 애니메이션 실행 책임 이전
    모델이 아닌 View Model 가 가져야 할 책임인 UI 애니메이션 실행을 모델이 갖고 있어 View Model로 이전.
    스크립트에서는 View Model을 Controller로 해두긴 했음.

--- Assets/Scripts/Actor/Skill/ISkill.cs
+++ Assets/Scripts/Actor/Skill/ISkill.cs
@@ -1,26 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Fusion;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public abstract class ISkill : NetworkBehaviour
 {
+    protected ActorStat _stat;
     //UI 애니메이션
-    public Animator _anim;
+    [CanBeNull] public Animator _anim;
     public Sprite _icon{ get; protected set; }
     public float _coolTime{ get; protected set; }
     [Networked] public bool _canUse { get; set; } = true;
+    public Action<float> SkillStart = null;
+    public Action CoolTimeEnd = null;
     public abstract void Activate();
 
-    //너무 결합도 높은가 싶음... 쿨타임 UI를 그냥 Action으로 옮기는 것도 괜찮을지도...
+    //너무 결합도 높은가 싶음... ㅋㅋ ㅠ
     protected IEnumerator CoolDown(float coolTime)
     {
+        //_anim이 null일 수 있고, 이건 확실히... 다른 쪽으로 넘기는 게 맞음.
+        float realCoolTime = coolTime * _stat.CoolTimePercent.Value;
         if(HasStateAuthority) _canUse = false;
-        _anim.speed = 1 / coolTime;
-        _anim.Play("CoolTime");
-        yield return new WaitForSeconds(coolTime);
+        SkillStart?.Invoke(realCoolTime);
+        yield return new WaitForSeconds(realCoolTime);
         if(HasStateAuthority) _canUse = true;
-        _anim.Play("New State");
+        CoolTimeEnd?.Invoke();
     }
 }
