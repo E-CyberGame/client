6212ed3a5d402fe28ca000edfccc434478612b7a
강승연 <syeon.dev@gmail.com>
2024-10-14 17:17:33 +0900

    [REFACTOR] 발사체 유형 스킬 발사체 저장 리스트 삭제
    * 발사체 Instantiate 후 List에 저장하던 로직 삭제
    * 발사체 생성 후 반환해 해당 반환값을 이용하는 것으로 변경

--- Assets/Scripts/Actor/Skill/ChainFireBall.cs
+++ Assets/Scripts/Actor/Skill/ChainFireBall.cs
@@ -18,28 +18,27 @@ namespace Actor.Skill
             _buff = GetComponent<BuffController>();
             _icon = Resources.Load<Sprite>("SkillIcon/ChainFireBall");
             _coolTime = 4f;
-            
+            goList.Add(Resources.Load<GameObject>("TestPrefabs/Fireball"));
         }
         //Activate에서 can use 막기
         public override void Activate()
         {
             if (!_canUse) return;
             base.Activate();
-            int id = GetSkillId();
-            projectileList[id] = new List<Projectile>();
-            StartCoroutine(Chain(id));
+            StartCoroutine(Chain());
             _buff.AddBuff(new MaxHPUpBuff(_stat));
         }
         
-        IEnumerator Chain(int id)
+        IEnumerator Chain()
         {
             Vector3 primaryPosition = _stat.transform.position;
             Vector3 primaryDirection = _body.currentDirectionX;
             for (int i = 0; i < 3; i++)
-            {
-                Generate(id, 0);
-                projectileList[id][i].Init(_stat, primaryDirection, primaryPosition, new Vector3(1f, 0.7f, 0f), 1.2f, new Vector3(10f, 0f, 0f));
-                projectileList[id][i].Fire();
+            { 
+                Projectile ball = Generate(0);
+                if (ball is null) continue;
+                ball.Init(_stat, primaryDirection, primaryPosition, new Vector3(1f, 0.7f, 0f), 1.2f, new Vector3(10f, 0f, 0f));
+                ball.Fire();
                 yield return new WaitForSeconds(0.7f);
             }
         }
