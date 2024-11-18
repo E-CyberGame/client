d60a207e7c449514df933f104c4de3200597cf5f
Song Ein <songein@ewhain.net>
2024-11-18 17:52:44 +0900

    [1118]: 캐릭터 매니징 창 오염도 관련 버튼 UI
    - 크리스탈 소모하여 오염도를 깎는 로직의 시각적인 처리

--- Assets/Scripts/UI/UI_CharacterManaging.cs
+++ Assets/Scripts/UI/UI_CharacterManaging.cs
@@ -9,7 +9,12 @@ using UnityEngine.SceneManagement;
 
 public class UI_CharacterManaging : UI_Popup
 {
+    private int spend_crystal;
+    private int poll_rate = 99;
     private GameObject pollutionPopup;
+    [SerializeField] private TextMeshProUGUI cost_text;
+    [SerializeField] private TextMeshProUGUI after_poll;
+    [SerializeField] private TextMeshProUGUI pollusion;
     enum Buttons
     {
         BackButton,
@@ -21,7 +26,8 @@ public class UI_CharacterManaging : UI_Popup
         */
         Skill1, Skill2, Skill3, Skill4, Skill5,
         PollutionClearButton,
-        PollutionPopupCancle, PollutionPopupAccept
+        PollutionPopupCancle, PollutionPopupAccept,
+        Plus10Button, Minus10Button, Plus100Button,
     }
 
     private void Start()
@@ -49,6 +55,9 @@ public class UI_CharacterManaging : UI_Popup
         GetButton((int)Buttons.PollutionClearButton).gameObject.BindUIEvent(PollutionClearButtonClicked);
         GetButton((int)Buttons.PollutionPopupCancle).gameObject.BindUIEvent(PollutionPopupCancleClicked);
         GetButton((int)Buttons.PollutionPopupAccept).gameObject.BindUIEvent(PollutionPopupAcceptClicked);
+        GetButton((int)Buttons.Plus10Button).gameObject.BindUIEvent(AddCrystal10);
+        GetButton((int)Buttons.Minus10Button).gameObject.BindUIEvent(ReduceCrystal10);
+        GetButton((int)Buttons.Plus100Button).gameObject.BindUIEvent(AddCrystal100);
 
     }
     public void BackButtonClicked(PointerEventData eventData)
@@ -78,6 +87,31 @@ public class UI_CharacterManaging : UI_Popup
     public void PollutionPopupAcceptClicked(PointerEventData eventData)
     {
         Debug.Log("PollutionPopupAccept Clicked");
+        pollusion.text = poll_rate.ToString() + "%";
         pollutionPopup.SetActive(false);
+    }
+    public void AddCrystal10(PointerEventData eventData)
+    {
+        if (poll_rate - 1 <= 0) return;
+        spend_crystal += 10;
+        poll_rate -= 1;
+        cost_text.text = spend_crystal.ToString();
+        after_poll.text = poll_rate.ToString() + "%";
+    }
+    public void AddCrystal100(PointerEventData eventData)
+    {
+        if (poll_rate - 10 <= 0) return;
+        spend_crystal += 100;
+        poll_rate -= 10;
+        cost_text.text = spend_crystal.ToString();
+        after_poll.text = poll_rate.ToString() + "%";
+    }
+    public void ReduceCrystal10(PointerEventData eventData)
+    {
+        if (spend_crystal <= 0) return;
+        spend_crystal -= 10;
+        poll_rate += 1;
+        cost_text.text = spend_crystal.ToString();
+        after_poll.text = poll_rate.ToString() + "%";
     }
 }
