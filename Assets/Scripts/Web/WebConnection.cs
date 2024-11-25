f61b969bbbb65344a7f21b0a90febbcf4fafe530
강승연 <syeon.dev@gmail.com>
2024-11-25 09:59:44 +0900

    [UPDATE] 웹서버 연결 범용적으로 사용 가능하도록 개선(WebConnection)

--- Assets/Scripts/Web/WebConnection.cs
+++ Assets/Scripts/Web/WebConnection.cs
@@ -8,23 +8,35 @@ namespace Web
 {
     public class WebConnection : MonoBehaviour
     {
-        private const string _endpoint = "http://localhost:8080/api/";
+        private const string _endpoint = "http://18.224.19.4:8080/api/";
+
+        public void SentGet<T>(string request, Action<T> successAction, Action failAction)
+        {
+            StartCoroutine(SendGetRequest<T>(request, successAction, failAction));
+        }
         
+        public void SentGet(string request, Action<string> successAction, Action failAction)
+        {
+            StartCoroutine(SendGetRequest(request, successAction, failAction));
+        }
+        
+        public void SendPost<T>(string request, DTO.DTO data, Action<T> successAction, Action failAction = null)
+        {
+            string body = JsonConvert.SerializeObject(data);
+            StartCoroutine(SendPostRequest<T>(request, body, successAction, failAction));
+        }
+
         public void SendPost(string request, DTO.DTO data, Action<string> successAction, Action failAction = null)
         {
             string body = JsonConvert.SerializeObject(data);
-            StartCoroutine(SendRequest(request, body, successAction, failAction));
+            StartCoroutine(SendPostRequest(request, body, successAction, failAction));
         }
         
-        private IEnumerator SendRequest(string request, string json, Action<string> successAction, Action failAction = null)
+        #region private
+        private IEnumerator SendGetRequest<T>(string request, Action<string> successAction, Action failAction = null)
         {
-            using (UnityWebRequest webRequest = new UnityWebRequest(_endpoint + request, "POST"))
+            using (UnityWebRequest webRequest = new UnityWebRequest(_endpoint + request, "GET"))
             {
-                byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
-                webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
-                webRequest.downloadHandler = new DownloadHandlerBuffer();
-                webRequest.SetRequestHeader("Content-Type", "application/json");
-
                 // 요청 보내기
                 yield return webRequest.SendWebRequest();
 
@@ -35,7 +47,7 @@ namespace Web
                 }
                 else
                 {
-                    if (webRequest.downloadHandler.text.Contains("[Fail]"))
+                    if (webRequest.downloadHandler.text.Contains("fail"))
                     {
                         failAction?.Invoke();
                     }
@@ -47,5 +59,119 @@ namespace Web
                 }
             }
         }
+
+        private IEnumerator SendGetRequest<T>(string request, Action<T> successAction, Action failAction = null)
+        {
+            using (UnityWebRequest webRequest = new UnityWebRequest(_endpoint + request, "GET"))
+            {
+                // 요청 보내기
+                yield return webRequest.SendWebRequest();
+
+                if (webRequest.result != UnityWebRequest.Result.Success)
+                {
+                    failAction?.Invoke();
+                    Debug.Log($"Error: {webRequest.error}");
+                }
+                else
+                {
+                    if (webRequest.downloadHandler.text.Contains("fail"))
+                    {
+                        failAction?.Invoke();
+                    }
+                    else
+                    {
+                        try
+                        {
+                            T response = JsonConvert.DeserializeObject<T>(webRequest.downloadHandler.text);
+                            successAction?.Invoke(response);
+                        }
+                        catch (JsonSerializationException ex)
+                        {
+                            Debug.Log("직렬화 과정에서 오류 발생");
+                        }
+                    }
+                    Debug.Log("Response: " + webRequest.downloadHandler.text);
+                }
+            }
+        }
+        
+        private IEnumerator SendPostRequest<T>(string request, string json, Action<T> successAction, Action failAction = null)
+        {
+            using (UnityWebRequest webRequest = new UnityWebRequest(_endpoint + request, "POST"))
+            {
+                byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
+                webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+                webRequest.downloadHandler = new DownloadHandlerBuffer();
+                webRequest.SetRequestHeader("Content-Type", "application/json");
+                
+                Debug.Log("data : " + json);
+                Debug.Log("url : " + _endpoint + request);
+
+                // 요청 보내기
+                yield return webRequest.SendWebRequest();
+
+                if (webRequest.result != UnityWebRequest.Result.Success)
+                {
+                    failAction?.Invoke();
+                    Debug.Log($"Error: {webRequest.error}");
+                }
+                else
+                {
+                    if (webRequest.downloadHandler.text.Contains("fail"))
+                    {
+                        failAction?.Invoke();
+                    }
+                    else
+                    {
+                        try
+                        {
+                            T response = JsonConvert.DeserializeObject<T>(webRequest.downloadHandler.text);
+                            successAction?.Invoke(response);
+                        }
+                        catch (JsonSerializationException ex)
+                        {
+                            Debug.Log("직렬화 과정에서 오류 발생");
+                        }
+                    }
+                    Debug.Log("Response: " + webRequest.downloadHandler.text);
+                }
+            }
+        }
+        
+        private IEnumerator SendPostRequest(string request, string json, Action<string> successAction, Action failAction = null)
+        {
+            using (UnityWebRequest webRequest = new UnityWebRequest(_endpoint + request, "POST"))
+            {
+                byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
+                webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+                webRequest.downloadHandler = new DownloadHandlerBuffer();
+                webRequest.SetRequestHeader("Content-Type", "application/json");
+
+                Debug.Log("url : " + _endpoint + request);
+
+                // 요청 보내기
+                yield return webRequest.SendWebRequest();
+
+                if (webRequest.result != UnityWebRequest.Result.Success)
+                {
+                    failAction?.Invoke();
+                    Debug.Log($"Error: {webRequest.error}");
+                }
+                else
+                {
+                    if (webRequest.downloadHandler.text.Contains("fail"))
+                    {
+                        failAction?.Invoke();
+                    }
+                    else
+                    {
+                        successAction?.Invoke(webRequest.downloadHandler.text);
+                    }
+                    Debug.Log("Response: " + webRequest.downloadHandler.text);
+                }
+            }
+        }
+        
+        #endregion
     }
 }
